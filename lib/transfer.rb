b9257e7d95723fa91cca8e7f365adcaa97a2b452
Aaron Brady <aleebrady@gmail.com>
2020-08-13 04:34:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
-  # your code here
+  
+  attr_reader :transfer
+  attr_accessor :sender, :receiver, :status, :amount 
+  
+  def initialize(sender, receiver, amount)
+    @transfer = transfer
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  
+  
 end
